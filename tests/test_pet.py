508865b071f1b27b11afcd6300e49ff9b8dd82ba
VeronicaKo <VeronicaKo1987@gmail.ru>
2021-09-23 19:04:57 +0300

    all tests are ready except test_upload_image

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -1,33 +1,42 @@
 import pytest
 import base
 from mimesis import Person
+from PIL import Image
 
 
+@pytest.mark.pet1
 def test_upload_image():
-    base.upload_image()
+    file = Image.open('dog.jpg')
+    base.upload_image('11', file)
+    # if add_img.status_code < 400:
+    #     print(f'Pet № {id_pet} added. Status code is {stat_code}')
+    # else:
+    #     print(f'Pet № {id_pet} is NOT added. Status code is {stat_code}')
+    #     print(add_img.headers)
+    #     print(add_img.text)
 
 
 def test_add_new_pet():
     for i in range(2, 10):
-        base.add_new_pet(str(i), Person('en').name())
+        assert base.add_new_pet(str(i), Person('en').name()) < 400, f"Not added pet № {i}"
 
 
 def test_update_and_existing_pet():
-    base.update_an_existing_pet('2', 'shushu')
+    assert base.update_an_existing_pet('2', 'shushu') < 400, f"Not updated pet № {2}"
 
 
 def test_find_pet_by_status():
     stat = ['pending', 'available', 'sold']
     for i in stat:
-        base.find_pet_by_status(i)
+        assert base.find_pet_by_status(i) < 400, f'Pet with status {i} is NOT found'
 
 
 def test_find_pet_by_id():
-    base.find_pet_by_id('3')
+    assert base.find_pet_by_id('1') < 400, f'Pet № {1} is NOT found.'
 
 
 def test_update_pet_with_data():
-    base.update_pet_with_data('2', 'ricki', 'sold')
+    assert base.update_pet_with_data('2', 'ricki', 'sold') < 400, f'Pet № {2} is NOT updated'
 
 
 def test_deletes_pet():
